d8047eefd5ada77add620a0123902ec9849b4917
Alexandre Haristoy <alexandre.haristoy@gmail.com>
2020-04-10 02:58:48 +0200

    update

--- src/simple_navigation_goals/src/simple_navigation_goals.py
+++ src/simple_navigation_goals/src/simple_navigation_goals.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python
 import rospy
+import math
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal # Brings in the .action file and messages used by the move base action
-
+from geometry_msgs.msg import Quaternion
+from tf.transformations import quaternion_from_euler
 
 class SimpleNavigationGoals():
 
     def __init__(self):
-    
         rospy.init_node('simple_navigation_goals')
-
-        # Create an action client called "move_base" with action definition file "MoveBaseAction
-        self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-        rospy.loginfo("Waiting for move_base action server...")
-        wait = self.client.wait_for_server(rospy.Duration(5.0))
-        if not wait:
-            rospy.logerr("Action server not available !")
-            rospy.signal_shutdown("Action server not available !")
-            return
-        rospy.loginfo("Connecting to move base server")
-        rospy.loginfo("Starting goals achievements")
-        print("ok")
 
 
     def go_to(self, x, y, theta):
+        client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+        client.wait_for_server()
+
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
+
         goal.target_pose.pose.position.x = x
         goal.target_pose.pose.position.y = y
         goal.target_pose.pose.position.z = 0
+        q = quaternion_from_euler(0.0, 0.0, theta)
+        goal.target_pose.pose.orientation = Quaternion(*q)
+
         rospy.loginfo("sending goal pose "+str(goal.target_pose.pose.position.x)+
-        ", "+str(goal.target_pose.pose.position.y)+", "+str(goal.target_pose.pose.position.z)+ "to action server")
-        self.client.send_goal(goal)
-        wait = self.client.wait_for_result()
+        ", "+str(goal.target_pose.pose.position.y)+", "+str(goal.target_pose.pose.orientation)+ " to action server")
+        client.send_goal(goal)
+        wait = client.wait_for_result()
         if not wait:
             rospy.logerr("Action server not available !")
             rospy.signal_shutdown("Action server not available !")
         else:
-            return self.client.get_result()
+            return client.get_result()
 
-nav = SimpleNavigationGoals()
+    
+    def _shutdown(self):
+        rospy.signal_shutdown("Shutdown")
+
+
+s = SimpleNavigationGoals()
+s.go_to(0, 0, 0)
